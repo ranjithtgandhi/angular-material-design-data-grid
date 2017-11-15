0c7af4fbdc6ae6023487358a3878f36619c26920
Ranjith <ranjithtgandhi@gmail.com>
2017-11-15 09:42:46 +0530

    pagination issue - fixed

---
+++ app.js
@@ -0,0 +1,19 @@
+(function() {
+ 'use strict';
+
+	angular.module('mainApp', ["ui.router",'ui.bootstrap','angularUtils.directives.dirPagination','ngMaterial',"ngMessages","mainControllers","mainServices"])
+	.config(function($stateProvider, $urlRouterProvider) {	 	
+	 	
+		 	$stateProvider.state("list", {
+		 		url: "/",
+		 		templateUrl: "views/list.html",
+		 		controller: "mainCtrl"
+		 	});
+		 	$urlRouterProvider.otherwise("/");
+
+	})
+	.run(function($rootScope){
+		$rootScope.currentYear = new Date().getFullYear();
+	});
+
+})();
